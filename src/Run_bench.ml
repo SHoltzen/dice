7e263de67fa9042a3dacc64cc38433b46ee025d5
Steven Holtzen <sholtzen@gmail.com>
2020-01-16 12:24:14 -0800

    small bug with benchmark namespace

--- src/Run_bench.ml
+++ src/Run_bench.ml
@@ -1,7 +1,7 @@
 (* Documentation on the benchmark module:
    https://ocaml.janestreet.com/ocaml-core/latest/doc/core_bench/Core_bench/Bench/index.html *)
 open Core
-open Core_bench.Std
+open Core_bench
 open CoreGrammar
 open Wmc
 open BddUtil
