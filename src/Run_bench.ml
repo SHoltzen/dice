529784f5d30a7d2394a585f9ca4cbb9b3141414b
Steven Holtzen <sholtzen@gmail.com>
2020-02-11 10:43:24 -0800

    experiment

--- src/Run_bench.ml
+++ src/Run_bench.ml
@@ -67,6 +67,45 @@ let bench_caesar inline_functions =
                        (LogProbability.to_string 10.0 numpaths) sz);
     )
 
+
+let gen_caesar_error (str: int list) =
+  let prog = ref "
+fun drawChar(key: int(26), observation: int(26)) {
+    let drawnChar = discrete(0.08167, 0.01492, 0.02782, 0.04253, 0.12702, 0.02228, 0.02015, 0.06094, 0.06966, 0.0153, 0.0772, 0.04025, 0.02406, 0.06749, 0.07507, 0.01929, 0.00095, 0.05987, 0.06327, 0.09056, 0.02758, 0.00978, 0.02360, 0.00150, 0.01974, 0.00074) in
+    let encrypted = key + drawnChar  in
+    let fail = flip 0.0001 in
+    if fail then (observe observation == drawnChar)
+    else (observe observation == encrypted)
+}
+let key1  = discrete(0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538,0.038461538) in
+" in
+  List.iter str ~f:(fun c ->
+      let new_ln = Format.sprintf "let a = drawChar(key1, int(26, %d)) in" c in
+      prog := Format.sprintf "%s\n%s" !prog new_ln;
+    );
+  prog := Format.sprintf "%s\n%s" !prog "key1";
+  parse_with_error (Lexing.from_string !prog)
+
+(** [bench_caesar_error] runs the Caesar cipher scaling benchmarks with errors
+    in the encryption. [inline_functions] is true if functions are inlined,
+    false otherwise *)
+let bench_caesar_error inline_functions =
+  Format.printf "Length\tTime (s)\tBDD Size\n";
+  let lst = List.init 50 ~f:(fun i -> i * 1000) in
+  List.iter lst ~f:(fun len ->
+      let t0 = Unix.gettimeofday () in
+      let caesar = gen_caesar (List.init len ~f:(fun i -> Random.int_incl 0 25)) in
+      let res = (if inline_functions then Passes.inline_functions caesar else caesar)
+                |> CoreGrammar.from_external_prog
+                |> CoreGrammar.compile_program in
+      let sz = Cudd.Bdd.size res.body.z in
+      let t1 = Unix.gettimeofday () in
+      let numpaths = Passes.num_paths caesar in
+      print_endline (Format.sprintf "%d\t%f\t%s\t%d" len (t1 -. t0)
+                       (LogProbability.to_string 10.0 numpaths) sz);
+    )
+
+
 let command =
   Command.basic
     ~summary:"Generate an MD5 hash of the input data"
@@ -74,6 +113,7 @@ let command =
     (let open Command.Let_syntax in
      let open Command.Param in
      let%map caesar = flag "-caesar" no_arg ~doc:" run caesar cipher scaling"
+     and caesar_error = flag "-caesar-error" no_arg ~doc:" run caesar cipher scaling"
      and baselines = flag "-baselines" no_arg ~doc:" run the baseline experiments"
      in fun () ->
        if baselines then (
@@ -83,7 +123,13 @@ let command =
          Format.printf "****************************************[Caesar No Inline]****************************************\n";
          bench_caesar false;
          Format.printf "****************************************[Caesar Inlined]****************************************\n";
-         bench_caesar true))
+         bench_caesar true);
+       if caesar_error then (
+         Format.printf "****************************************[Caesar No Inline]****************************************\n";
+         bench_caesar_error false;
+         Format.printf "****************************************[Caesar Inlined]****************************************\n";
+         bench_caesar_error true)
+    )
 
 let () =
   Command.run ~version:"1.0" command
