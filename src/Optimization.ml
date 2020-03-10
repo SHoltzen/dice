cfba182ef547a58b86445c4c7d64a6c251810622
ellie <ellieyhc@gmail.com>
2020-03-09 20:31:07 -0700

    Added return types and comments

--- src/Optimization.ml
+++ src/Optimization.ml
@@ -1,23 +1,27 @@
 open Core
 open ExternalGrammar
 
-let rec check item lst = 
+(* Return true if item is in lst *)
+let rec check item lst : bool = 
   match lst with
   | [] -> false
   | head::tail -> if (head = item) then true else (check item tail)
 
-let rec ckFlip l1 l2 = 
+  (* Return list of (variable, flip float) that appear in both lists *)
+let rec ckFlip l1 l2 : 'a list = 
   match l1 with
   | [] -> []
   | head::tail -> 
     (if (check head l2) then 
       (* let (v, f) = head in ( *)
       (* (Format.printf "let %s = flip %f\n" v f;  *)
-      (let hd::tl = l2 in
-      head::(ckFlip tail tl))
+      (match l2 with
+      | [] -> []
+      | hd::tl -> head::(ckFlip tail tl))
     else (ckFlip tail l2))
 
-let rec poHelp item e = 
+  (* Remove let x = flip f from e *)
+let rec poHelp item e : ExternalGrammar.eexpr = 
   match e with
   | Ite(g, thn, els) ->
     let t = poHelp item thn in 
@@ -31,36 +35,22 @@ let rec poHelp item e =
       Let(v, e1, (poHelp item e2)))
   | _ -> e
     
-let rec pullOut l1 expr = 
+  (* Pull let x = flip f out from if then else *)
+let rec pullOut l1 expr : ExternalGrammar.eexpr = 
   match l1 with
   | [] -> expr
   | head::[] -> let (v,f) = head in Let(v, Flip(f), (poHelp head expr))
   | head::tail -> let (v,f) = head in Let(v, Flip(f), (poHelp head (pullOut tail expr)))
 
-let cmp t1 t2 = 
+  (* Comparison function for sort. Sort by flip, breaking ties with variable name *)
+let cmp t1 t2 : int = 
   let (v1, f1) = t1 in
   let (v2, f2) = t2 in
   if f1 < f2 then -1 else if f1 > f2 then 1 else if v1 < v2 then -1 else if v1 > v2 then 1 else 0
 
-let rec helper (e: ExternalGrammar.eexpr) =
+  (* helper: external expr -> (list of (variable name, flip param). )*)
+let rec helper (e: ExternalGrammar.eexpr) : (string * float) list * ExternalGrammar.eexpr =
   match e with
-  | And(e1, e2)
-  | Or(e1, e2)
-  | Plus(e1, e2)
-  | Eq(e1, e2)
-  | Minus(e1, e2)
-  | Neq(e1, e2)
-  | Div(e1, e2)
-  | Mult(e1, e2)
-  | Lt(e1, e2)
-  | Gt(e1, e2)
-  | Lte(e1, e2)
-  | Gte(e1, e2)
-  | Tup(e1, e2) -> ([], e)
-  | Snd(e1) | Fst(e1)
-  | Not(e1)
-  | Observe(e1) -> ([], e)
-
   | Let(v, e1, e2)-> 
     let (s1, n1) = helper e1 in
     let (s2, n2) = helper e2 in
@@ -78,7 +68,10 @@ let rec helper (e: ExternalGrammar.eexpr) =
       let (l, n) = helper (pullOut can (Ite(g, n2, n3))) in
       ([], n))
 
-  | Flip(f) -> ([], e)
+  | And(_, _) | Or(_, _) | Plus(_, _) | Eq(_, _) | Minus(_, _)
+  | Neq(_, _) | Div(_, _) | Mult(_, _) | Lt(_, _) | Gt(_, _)
+  | Lte(_, _) | Gte(_, _) | Tup(_, _) -> ([], e)
+  | Snd(_) | Fst(_) | Not(_) | Observe(_) | Flip(_) -> ([], e)
   | _ -> ([], e)
 
 let rec print_expr (e: ExternalGrammar.eexpr) = 
@@ -89,16 +82,16 @@ let rec print_expr (e: ExternalGrammar.eexpr) =
     )
   | Let(x, e1, e2) ->
     (Format.printf "let %s = " x;
-    let tmp = (print_expr e1) in 
+    let _ = (print_expr e1) in 
     Format.printf "in \n";
-    let tmp = (print_expr e2) in e)
+    let _ = (print_expr e2) in e)
   | Ite(g, thn, els) -> 
     (Format.printf "if ";
-    let tmp = (print_expr g) in 
+    let _ = (print_expr g) in 
     Format.printf "then ";
-    let tmp = (print_expr thn) in 
+    let _ = (print_expr thn) in 
     Format.printf "else ";
-    let tmp = (print_expr els) in e)
+    let _ = (print_expr els) in e)
   | True -> Format.printf "True "; e
   | False -> Format.printf "False "; e
   | Ident(x) -> Format.printf "%s " x; e
